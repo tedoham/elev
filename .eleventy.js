e8cc2a45fd44728312761aa3a32dbc297463a056
tedoham <lotlovethio@gmail.com>
2020-04-05 14:47:11 +0300

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addPassthroughCopy("./src/js");
+    eleventyConfig.addPassthroughCopy("./src/img");
+    return {
+        markdownTemplateEngine: "njk",
+        dir: {
+            input: "src",
+            output: "dist"
+        }
+    };
+};
