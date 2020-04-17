bb61a049bd33dec01a7aad52a93c96594ac3dad7
tedoham <lotlovethio@gmail.com>
2020-04-17 12:26:58 +0300

    dynamic api added

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,11 +1,14 @@
-const axios = require('axios');
+import { get } from 'axios';
+require('dotenv').config();
 
-module.exports = async function() {
+export default async function() {
   try {
-    const response = await axios.get('-----');
+    const response = await get(
+      `http://newsapi.org/v2/everything?q=bitcoin&from=2020-03-17&sortBy=publishedAt&apiKey=${process.env.API_KEY}`
+    );
     console.log(response);
     return response.data;
   } catch (error) {
     console.error(error);
   }
-};
+}
