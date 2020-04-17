d000345ade24def4e41b2a9401c22cc66c8edb7a
tedoham <lotlovethio@gmail.com>
2020-04-17 15:44:01 +0300

    rewrites for multiple languages

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,14 +1,35 @@
 const axios = require('axios');
 require('dotenv').config();
+const countries = require('./countries.json');
 
-module.exports = async function() {
+async function getNews(country) {
   try {
     const response = await axios.get(
-      `http://newsapi.org/v2/everything?q=bitcoin&from=2020-03-17&sortBy=publishedAt&apiKey=${process.env.NEWS_API_KEY}`
+      `http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}`
     );
-    console.log(response);
-    return response.data;
+    return {
+      country: country,
+      articles: response.data.articles
+    };
   } catch (error) {
     console.error(error);
   }
+}
+
+module.exports = async function() {
+  var newsPromises = countries.map(getNews);
+  return Promise.all(newsPromises).then(newObjects => {
+    console.log('newObjects :-', newObjects);
+    return [].concat.apply([], newObjects);
+  });
 };
+
+// try {
+//   const response = await axios.get(
+//     `http://newsapi.org/v2/everything?q=bitcoin&from=2020-03-17&sortBy=publishedAt&apiKey=${process.env.NEWS_API_KEY}`
+//   );
+//   console.log(response);
+//   return response.data;
+// } catch (error) {
+//   console.error(error);
+// }
