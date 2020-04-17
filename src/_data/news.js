923d92619ac8b4fae3945893723862e5e3128feb
tedoham <lotlovethio@gmail.com>
2020-04-17 12:40:09 +0300

    some api updates

--- src/_data/news.js
+++ src/_data/news.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 module.exports = async function() {
   try {
     const response = await axios.get(
-      `http://newsapi.org/v2/everything?q=bitcoin&from=2020-03-17&sortBy=publishedAt&apiKey=${process.env.API_KEY}`
+      `http://newsapi.org/v2/everything?q=bitcoin&from=2020-03-17&sortBy=publishedAt&apiKey=${process.env.NEWS_API_KEY}`
     );
     console.log(response);
     return response.data;
